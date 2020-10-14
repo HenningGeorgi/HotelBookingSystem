7b85ac310c512c211185cf1e672e1aaa6e9d9caf
henni <henning.hg.georgi@t-online.de>
2020-10-14 15:16:03 +0200

    develop/cleanup

--- src/main/java/org/hbs/model/CreateBookingRequest.java
+++ src/main/java/org/hbs/model/CreateBookingRequest.java
@@ -3,14 +3,9 @@ package org.hbs.model;
 import java.time.LocalDate;
 
 public class CreateBookingRequest {
-<<<<<<< Updated upstream
-    private Guest guest;
 
-    public Guest getGuest() {
-=======
     private String guest;
     public String getGuest() {
->>>>>>> Stashed changes
         return guest;
     }
 
